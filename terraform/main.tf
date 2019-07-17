fc78aa84f729e246dcab22fb109969d5c751a548
sakethramanujam <ramanujamsamavedam@hotmail.com>
2019-07-17 15:42:54 +0530

    modify terraform file, change app.py in fib_2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "google_cloudbuild_trigger" "update-trigger" {
   description  = "Fibonocci Cloud Run Trigger"
   project = "terrific-terraform"
-
+  included_files = ["fibrun/*"]
   trigger_template {
     branch_name = "master"
     repo_name   = "github_sakethramanujam_terrific-terraform"
@@ -12,7 +12,7 @@ resource "google_cloudbuild_trigger" "update-trigger" {
 resource "google_cloudbuild_trigger" "update-trigger-2" {
   description  = "Fibonocci 2 Cloud Run Trigger"
   project = "terrific-terraform"
-
+  included_files = ["fibrun_2/*"]
   trigger_template {
     branch_name = "master"
     repo_name   = "github_sakethramanujam_terrific-terraform"
