bcd0f880eeaf3f09f4dd5f4ea666ffdb393b640a
sakethramanujam <ramanujamsamavedam@hotmail.com>
2019-07-17 15:17:31 +0530

    add duplicate project for build checking

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,14 @@ resource "google_cloudbuild_trigger" "update-trigger" {
   }
   filename = "fibrun/cloudbuild.yaml"
 }
+resource "google_cloudbuild_trigger" "update-trigger-2" {
+  description  = "Fibonocci 2 Cloud Run Trigger"
+  project = "terrific-terraform"
+
+  trigger_template {
+    branch_name = "master"
+    repo_name   = "github_sakethramanujam_terrific-terraform"
+    dir = "fibrun_2/"
+  }
+  filename = "fibrun_2/cloudbuild.yaml"
+}
